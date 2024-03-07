3ba358f3787d24d7fa279257cd4d4e79e2b8c69b
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-03-07 10:59:30 +0100

    predavanje 9

---
+++ predavanje9/index.js
@@ -0,0 +1,10 @@
+
+
+let age = 21;
+let student = false;
+
+let nameTag = document.getElementById("name")  
+nameTag.innerHTML = "Name: Marko";
+
+document.getElementById("age").innerHTML = `Age: ${age}`
+
